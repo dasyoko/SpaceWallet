650dcd14a0aaa201944c691c1589f011ad8f0783
dakoslug <dakoslug@gmail.com>
2018-03-03 13:53:52 -0500

    Added Wallet send prompt

--- space-wallet/src/components/mainpage/WalletDetails.js
+++ space-wallet/src/components/mainpage/WalletDetails.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
 import AppBar from '../AppBar';
 import RaisedButton from 'material-ui/RaisedButton/RaisedButton';
+import WalletPrompt from './dialogs/WalletPrompt';
 
 
 class WalletDetails extends Component {
     constructor(props) {
         super(props);
-        this.state = { dataLoaded : false, walletDetails : {}}
+        this.state = { dataLoaded : false, walletDetails : {}, prompt: false}
 
         /* Structure of wallet Details
             coin: balance of wallet
             wallet: Wallet address
         */
+    }
+    promptUser() {
+        this.setState({prompt: true})
     }
     render() {
         if(this.state.dataLoaded)
@@ -23,9 +27,10 @@ class WalletDetails extends Component {
                 <h1>Wallet Information</h1>
                 <h2>{this.state.walletDetails.coin}</h2>
                 <div className="financialDetails">
-                    <RaisedButton label="Send"/>
+                    <RaisedButton onClick={this.promptUser.bind(this)} label="Send"/>
                     <RaisedButton className="secondBtn" label="Check Transaction History"/>
                 </div>
+                <WalletPrompt open = {this.state.prompt} />
             </div>) 
         }
     }
---
+++ space-wallet/src/components/mainpage/dialogs/WalletPrompt.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react'
+import TextField from 'material-ui/TextField/TextField';
+import Dialog from 'material-ui/Dialog/'
+import FlatButton from 'material-ui/FlatButton'
+
+class WalletPrompt extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {wallet: "" , amount: 0, open: props.open}
+    }
+    componentWillReceiveProps(nextProps) {
+        this.setState({ open: nextProps.open }); 
+      }
+
+    handleOpen = () => {
+        this.setState({open: true});
+    };
+    
+    handleClose = () => {
+        this.setState({open: false});
+    };
+
+    walletChange(e) {
+        this.setState({wallet: e.target.value})
+    }
+    amountChange(e) {
+        this.setState({amount: e.target.value})
+    }
+    render() {
+        const actions = [
+            <FlatButton
+              label="Cancel"
+              primary={true}
+              onClick={this.handleClose}
+            />,
+            <FlatButton
+              label="Submit"
+              primary={true}
+              keyboardFocused={true}
+              onClick={this.handleClose} // To be changed later
+            />,
+          ];
+      return(
+        <div>
+        <Dialog
+          title="Wallet Payment"
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}>
+          Enter a wallet address:&nbsp;
+          <TextField onChange={this.walletChange.bind(this)}  hintText="Wallet Address"/>
+          <p/>Amount:&nbsp; 
+          <TextField onChange={this.amountChange.bind(this)}  hintText="Amount"/>
+        </Dialog>
+      </div>
+      )
+    }
+}
+
+export default WalletPrompt;
